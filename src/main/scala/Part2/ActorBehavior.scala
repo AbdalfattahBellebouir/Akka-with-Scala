f524ee1b5eaefd5ac621d06a27c609e3e4ace962
AbdalfattahBellebouir <abdalfbellebouir@gmail.com>
2020-10-30 17:57:41 +0100

    update Actor Behavior

--- src/main/scala/Part2/ActorBehavior.scala
+++ src/main/scala/Part2/ActorBehavior.scala
@@ -1,5 +1,26 @@
 package Part2
 
+import akka.actor.{Actor, ActorRef}
+
 object ActorBehavior extends App{
+  case class Vote(candidate: String)
+  case object VoteStatusRequest
+  case class VoteStatusReply(cadidate: Option[String])
+  class Citizen extends Actor{
+    override def receive: Receive = {
+      ???
+    }
+  }
+
+  case class AggregatorVotes(citizens: Set[ActorRef])
+  class VoteAggregator extends Actor{
+    override def receive: Receive = {
+      ???
+    }
+  }
+
+
+
+
 
 }
